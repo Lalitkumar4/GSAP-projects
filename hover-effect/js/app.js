276e9986ab2611eb0fe52b657ae3f836e670d237
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2023-06-09 18:06:47 +0530

    Initial commit

---
+++ hover-effect/js/app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "../img/img2.jpg",
+  image2: "../img/img1.jpg",
+  //angle: Math.PI / 8,
+  //speedIn: 1,
+  //speedOut: 5,
+  displacementImage: "../img/stripe1mul.png", //change image for different animation
+});
